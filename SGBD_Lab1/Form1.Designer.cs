dc2d060acf0527f514b043625c7d907499fe2df0
SebiB24 <132143076+SebiB24@users.noreply.github.com>
2025-03-09 00:11:09 +0200

    select + view and insert

--- SGBD_Lab1/Form1.Designer.cs
+++ SGBD_Lab1/Form1.Designer.cs
@@ -33,12 +33,8 @@ namespace SGBD_Lab1
             dataGridView1 = new DataGridView();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
-            textBox3 = new TextBox();
-            textBox4 = new TextBox();
             label1 = new Label();
             label2 = new Label();
-            label3 = new Label();
-            label4 = new Label();
             button1 = new Button();
             button2 = new Button();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
@@ -67,20 +63,6 @@ namespace SGBD_Lab1
             textBox2.Size = new Size(183, 27);
             textBox2.TabIndex = 2;
             // 
-            // textBox3
-            // 
-            textBox3.Location = new Point(569, 185);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(183, 27);
-            textBox3.TabIndex = 3;
-            // 
-            // textBox4
-            // 
-            textBox4.Location = new Point(569, 244);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(183, 27);
-            textBox4.TabIndex = 4;
-            // 
             // label1
             // 
             label1.AutoSize = true;
@@ -88,7 +70,7 @@ namespace SGBD_Lab1
             label1.Name = "label1";
             label1.Size = new Size(50, 20);
             label1.TabIndex = 5;
-            label1.Text = "label1";
+            label1.Text = "Nume";
             // 
             // label2
             // 
@@ -97,34 +79,17 @@ namespace SGBD_Lab1
             label2.Name = "label2";
             label2.Size = new Size(50, 20);
             label2.TabIndex = 6;
-            label2.Text = "label2";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(496, 185);
-            label3.Name = "label3";
-            label3.Size = new Size(50, 20);
-            label3.TabIndex = 7;
-            label3.Text = "label3";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(496, 244);
-            label4.Name = "label4";
-            label4.Size = new Size(50, 20);
-            label4.TabIndex = 8;
-            label4.Text = "label4";
+            label2.Text = "Nr. locatii";
             // 
             // button1
             // 
-            button1.Location = new Point(496, 303);
+            button1.Location = new Point(496, 187);
             button1.Name = "button1";
             button1.Size = new Size(201, 73);
             button1.TabIndex = 9;
-            button1.Text = "button1";
+            button1.Text = "Add";
             button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
             // 
             // button2
             // 
@@ -132,7 +97,7 @@ namespace SGBD_Lab1
             button2.Name = "button2";
             button2.Size = new Size(182, 43);
             button2.TabIndex = 10;
-            button2.Text = "button2";
+            button2.Text = "Connect";
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
@@ -143,12 +108,8 @@ namespace SGBD_Lab1
             ClientSize = new Size(800, 450);
             Controls.Add(button2);
             Controls.Add(button1);
-            Controls.Add(label4);
-            Controls.Add(label3);
             Controls.Add(label2);
             Controls.Add(label1);
-            Controls.Add(textBox4);
-            Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
             Controls.Add(dataGridView1);
@@ -164,12 +125,8 @@ namespace SGBD_Lab1
         private DataGridView dataGridView1;
         private TextBox textBox1;
         private TextBox textBox2;
-        private TextBox textBox3;
-        private TextBox textBox4;
         private Label label1;
         private Label label2;
-        private Label label3;
-        private Label label4;
         private Button button1;
         private Button button2;
     }
--- SGBD_Lab1/Form1.cs
+++ SGBD_Lab1/Form1.cs
@@ -10,16 +10,15 @@ namespace SGBD_Lab1
         Integrated Security=True;TrustServerCertificate=true;"; // crating connection string for SQL DB
         DataSet dataSet = new DataSet();
         SqlDataAdapter dataAdaptor = new SqlDataAdapter();
+        SqlConnection con;
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void DisplayInView(SqlConnection con)
+        private void DisplayInView()
         {
-            // citirea datelor
-            dataAdaptor.SelectCommand = new SqlCommand("SELECT * FROM Lant;", con);
             dataSet.Clear();
             dataAdaptor.Fill(dataSet);
             dataGridView1.DataSource = dataSet.Tables[0];
@@ -28,20 +27,40 @@ namespace SGBD_Lab1
         }
 
         private void button2_Click(object sender, EventArgs e)
-        { 
+        {
             try
             {
-                using (SqlConnection con = new SqlConnection(conectionString))
-                {
-                    con.Open();
-                    MessageBox.Show("Starea conexiunii: " + con.State);
-                    con.Close();
-                    DisplayInView(con);
-                }
+                con = new SqlConnection(conectionString);
+                con.Open();
+                ///MessageBox.Show("Starea conexiunii: " + con.State);
+
+                // select + display
+                dataAdaptor.SelectCommand = new SqlCommand("SELECT * FROM Lant", con);
+                DisplayInView();
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
+                con.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                dataAdaptor.InsertCommand = new SqlCommand("INSERT INTO Lant (nume, numar_locatii)" +
+                    " VALUES (@nume, @numar_locatii);", con);
+                dataAdaptor.InsertCommand.Parameters.AddWithValue("@nume", textBox1.Text);
+                dataAdaptor.InsertCommand.Parameters.AddWithValue("@numar_locatii", Int32.Parse(textBox2.Text));
+                dataAdaptor.InsertCommand.ExecuteNonQuery();
+                MessageBox.Show("Inserted Succesfully to  the Data Base");
+                DisplayInView();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                con.Close();
             }
         }
     }
