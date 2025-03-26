c5dc0e3c6723a1ee01a84ddec38d76c672dd3469
SebiB24 <132143076+SebiB24@users.noreply.github.com>
2025-03-26 23:46:01 +0200

    2 tablele cu legatura de fk

--- SGBD_Lab1/Form1.Designer.cs
+++ SGBD_Lab1/Form1.Designer.cs
@@ -30,104 +30,177 @@ namespace SGBD_Lab1
         /// </summary>
         private void InitializeComponent()
         {
-            dataGridView1 = new DataGridView();
+            dataGridViewParent = new DataGridView();
+            dataGridViewChild = new DataGridView();
             textBox1 = new TextBox();
             textBox2 = new TextBox();
+            textBox3 = new TextBox();
             label1 = new Label();
             label2 = new Label();
-            button1 = new Button();
-            button2 = new Button();
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            label3 = new Label();
+            addButton = new Button();
+            updateButton = new Button();
+            deleteButton = new Button();
+            label4 = new Label();
+            label5 = new Label();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewParent).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewChild).BeginInit();
             SuspendLayout();
             // 
-            // dataGridView1
+            // dataGridViewParent
             // 
-            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView1.Location = new Point(12, 81);
-            dataGridView1.Name = "dataGridView1";
-            dataGridView1.RowHeadersWidth = 51;
-            dataGridView1.Size = new Size(427, 357);
-            dataGridView1.TabIndex = 0;
+            dataGridViewParent.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewParent.Location = new Point(27, 40);
+            dataGridViewParent.Name = "dataGridViewParent";
+            dataGridViewParent.RowHeadersWidth = 51;
+            dataGridViewParent.Size = new Size(453, 214);
+            dataGridViewParent.TabIndex = 0;
+            // 
+            // dataGridViewChild
+            // 
+            dataGridViewChild.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewChild.Location = new Point(27, 304);
+            dataGridViewChild.Name = "dataGridViewChild";
+            dataGridViewChild.RowHeadersWidth = 51;
+            dataGridViewChild.Size = new Size(634, 214);
+            dataGridViewChild.TabIndex = 10;
             // 
             // textBox1
             // 
-            textBox1.Location = new Point(569, 83);
+            textBox1.Location = new Point(681, 336);
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(183, 27);
+            textBox1.Size = new Size(225, 27);
             textBox1.TabIndex = 1;
             // 
             // textBox2
             // 
-            textBox2.Location = new Point(569, 134);
+            textBox2.Location = new Point(681, 472);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(183, 27);
+            textBox2.Size = new Size(153, 27);
             textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(681, 403);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(153, 27);
+            textBox3.TabIndex = 14;
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(496, 83);
+            label1.Location = new Point(681, 313);
             label1.Name = "label1";
-            label1.Size = new Size(50, 20);
+            label1.Size = new Size(55, 20);
             label1.TabIndex = 5;
-            label1.Text = "Nume";
+            label1.Text = "Adresa";
             // 
             // label2
             // 
             label2.AutoSize = true;
-            label2.Location = new Point(496, 134);
+            label2.Location = new Point(681, 449);
             label2.Name = "label2";
-            label2.Size = new Size(50, 20);
+            label2.Size = new Size(117, 20);
             label2.TabIndex = 6;
-            label2.Text = "Nr. locatii";
+            label2.Text = "Data deschidere";
             // 
-            // button1
+            // label3
             // 
-            button1.Location = new Point(496, 187);
-            button1.Name = "button1";
-            button1.Size = new Size(201, 73);
-            button1.TabIndex = 9;
-            button1.Text = "Add";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            label3.AutoSize = true;
+            label3.Location = new Point(681, 380);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 20);
+            label3.TabIndex = 13;
+            label3.Text = "Suprafata ";
             // 
-            // button2
+            // addButton
             // 
-            button2.Location = new Point(12, 23);
-            button2.Name = "button2";
-            button2.Size = new Size(182, 43);
-            button2.TabIndex = 10;
-            button2.Text = "Connect";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
+            addButton.Location = new Point(539, 57);
+            addButton.Name = "addButton";
+            addButton.Size = new Size(367, 45);
+            addButton.TabIndex = 9;
+            addButton.Text = "Add";
+            addButton.UseVisualStyleBackColor = true;
+            // 
+            // updateButton
+            // 
+            updateButton.Location = new Point(539, 119);
+            updateButton.Name = "updateButton";
+            updateButton.Size = new Size(367, 45);
+            updateButton.TabIndex = 11;
+            updateButton.Text = "Update";
+            updateButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            deleteButton.Location = new Point(539, 180);
+            deleteButton.Name = "deleteButton";
+            deleteButton.Size = new Size(367, 45);
+            deleteButton.TabIndex = 12;
+            deleteButton.Text = "Delete";
+            deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label4.Location = new Point(27, 273);
+            label4.Name = "label4";
+            label4.Size = new Size(86, 28);
+            label4.TabIndex = 15;
+            label4.Text = "LOCATII";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label5.Location = new Point(27, 9);
+            label5.Name = "label5";
+            label5.Size = new Size(291, 28);
+            label5.TabIndex = 16;
+            label5.Text = "LANTURI DE SALI DE FITNESS";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button2);
-            Controls.Add(button1);
+            ClientSize = new Size(950, 548);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(deleteButton);
+            Controls.Add(updateButton);
+            Controls.Add(dataGridViewChild);
+            Controls.Add(addButton);
             Controls.Add(label2);
             Controls.Add(label1);
             Controls.Add(textBox2);
             Controls.Add(textBox1);
-            Controls.Add(dataGridView1);
+            Controls.Add(dataGridViewParent);
             Name = "Form1";
             Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)dataGridViewParent).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridViewChild).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
-        private DataGridView dataGridView1;
+        private DataGridView dataGridViewParent;
+        private DataGridView dataGridViewChild;
         private TextBox textBox1;
         private TextBox textBox2;
         private Label label1;
         private Label label2;
-        private Button button1;
-        private Button button2;
+        private Button addButton;
+        private Button updateButton;
+        private Button deleteButton;
+        private Label label3;
+        private TextBox textBox3;
+        private Label label4;
+        private Label label5;
     }
 }
--- SGBD_Lab1/Form1.cs
+++ SGBD_Lab1/Form1.cs
@@ -6,45 +6,49 @@ namespace SGBD_Lab1
 {
     public partial class Form1 : Form
     {
-        String conectionString = @"Server=LEGIONAR\SQLEXPRESS;Database=Lab1_SGBD;
+        String connectionString = @"Server=LEGIONAR\SQLEXPRESS;Database=Lab1_SGBD;
         Integrated Security=True;TrustServerCertificate=true;"; // crating connection string for SQL DB
-        DataSet dataSet = new DataSet();
-        SqlDataAdapter dataAdaptor = new SqlDataAdapter();
-        SqlConnection con;
+        DataSet ds = new DataSet();
+        SqlDataAdapter parentAdapter = new SqlDataAdapter();
+        SqlDataAdapter childAdapter = new SqlDataAdapter();
+        BindingSource bsParent = new BindingSource();
+        BindingSource bsChild = new BindingSource();
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void DisplayInView()
-        {
-            dataSet.Clear();
-            dataAdaptor.Fill(dataSet);
-            dataGridView1.DataSource = dataSet.Tables[0];
-
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
             try
             {
-                con = new SqlConnection(conectionString);
-                con.Open();
-                ///MessageBox.Show("Starea conexiunii: " + con.State);
-
-                // select + display
-                dataAdaptor.SelectCommand = new SqlCommand("SELECT * FROM Lant", con);
-                DisplayInView();
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    con.Open();
+                    parentAdapter.SelectCommand = new SqlCommand("SELECT * FROM Lant;", con);
+                    childAdapter.SelectCommand = new SqlCommand("SELECT * FROM Locatie;", con);
+                    parentAdapter.Fill(ds, "Lant");
+                    childAdapter.Fill(ds, "Locatie");
+                    DataColumn pkColumn = ds.Tables["Lant"].Columns["id_lant"];
+                    DataColumn fkColumn = ds.Tables["Locatie"].Columns["id_lant"];
+                    DataRelation relation = new DataRelation("FK_Lant_Locatii", pkColumn, fkColumn, true);
+                    ds.Relations.Add(relation);
+                    bsParent.DataSource = ds.Tables["Lant"];
+                    dataGridViewParent.DataSource = bsParent;
+                    textBox1.DataBindings.Add("Text", bsParent, "nume", true);
+                    bsChild.DataSource = bsParent;
+                    bsChild.DataMember = "FK_Lant_Locatii";
+                    dataGridViewChild.DataSource = bsChild;
+                }
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
-                con.Close();
             }
         }
 
+        /*
         private void button1_Click(object sender, EventArgs e)
         {
             try
@@ -63,5 +67,6 @@ namespace SGBD_Lab1
                 con.Close();
             }
         }
+        */
     }
 }
